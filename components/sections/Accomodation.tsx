1415096d6e041ba9f3274ef57c14a73ba9f2ba89
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-18 20:49:20 +0800

    revert

--- components/sections/Accomodation.tsx
+++ components/sections/Accomodation.tsx
@@ -4,33 +4,63 @@ import { merriweather } from "@/lib/font";
 import InfiniteScrollList from "@/components/ui/InfinityScrollLists";
 
 const images = [
-  { src: "/Images/room-1.png", alt: "Room 1", width: 240, height: 240 },
-  { src: "/Images/room-2.png", alt: "Room 2", width: 240, height: 240 },
-  { src: "/Images/room-3.png", alt: "Room 3", width: 240, height: 240 },
-  { src: "/Images/room-4.png", alt: "Room 4", width: 240, height: 240 },
-  { src: "/Images/room-5.png", alt: "Room 5", width: 240, height: 240 },
-  { src: "/Images/room-6.png", alt: "Room 6", width: 240, height: 240 },
+  {
+    src: "/Images/room-1.png",
+    alt: "Room 1",
+    width: 240,
+    height: 240,
+  },
+  {
+    src: "/Images/room-2.png",
+    alt: "Room 2",
+    width: 240,
+    height: 240,
+  },
+  {
+    src: "/Images/room-3.png",
+    alt: "Room 3",
+    width: 240,
+    height: 240,
+  },
+  {
+    src: "/Images/room-4.png",
+    alt: "Room 4",
+    width: 240,
+    height: 240,
+  },
+  {
+    src: "/Images/room-5.png",
+    alt: "Room 5",
+    width: 240,
+    height: 240,
+  },
+  {
+    src: "/Images/room-6.png",
+    alt: "Room 6",
+    width: 240,
+    height: 240,
+  },
 ];
 
 export default function AccomodationSection() {
   return (
-    <div className="relative mt-8 sm:mt-16 w-screen overflow-hidden">
-      <div className="absolute top-0 left-0 w-screen overflow-hidden -z-10">
-        <div className="absolute top-0 w-full h-2 bg-white z-10"></div>
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 160"
-          preserveAspectRatio="none"
-          className="block w-full"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V-2H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            fill="white"
-          />
-        </svg>
+    <div className="relative mt-8 sm:mt-16">
+      <div className="relative left-1/2 -translate-x-1/2 w-screen">
+        <div className="custom-shape-divider-top-1754616394 bg-blue-800">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 160"
+            preserveAspectRatio="none"
+            className="block w-full -mt-4 -mb-2"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              fill="white"
+            ></path>
+          </svg>
+        </div>
       </div>
-
       <div className="relative flex flex-col left-1/2 -translate-x-1/2 w-screen py-8 sm:py-16 px-6 lg:px-[280px] sm:px-6 gap-8 text-amber-500 bg-gradient-to-b from-blue-800 via-blue-400 to-transparent">
         <div className="flex flex-col gap-8">
           <h2 className={`${merriweather.className} text-3xl sm:text-4xl`}>
@@ -40,7 +70,6 @@ export default function AccomodationSection() {
             Comfort, space, and a view to remember.
           </p>
         </div>
-
         <div
           className="relative flex flex-col gap-4 w-full"
           style={{
@@ -60,4 +89,5 @@ export default function AccomodationSection() {
       </div>
     </div>
   );
+  ``;
 }
